99459e5dc4f6fb0c0e30f4edc29a67109226be33
Joshua Clements <josh@penpow.dev>
2021-10-02 12:11:25 +0100

    feat(deployment): Add in deploy command

---
+++ src/interactions/misc/deploy.ts
@@ -0,0 +1,45 @@
+import { CommandInteraction } from 'discord.js';
+import Client from "../../structures/Client";
+import Command from "../../structures/Command";
+import { ErrorTypes } from "../../types/ClientTypes";
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v9';
+
+export default class DeployCommand extends Command {
+    constructor(client: Client) {
+		super(client, {
+			name: 'deploy',
+			description: 'Deploy Slash Commands',
+			type: client.types.MISC,
+		});
+	}
+
+    async run(interaction: CommandInteraction): Promise<void> {
+        if(interaction.user.id !== '207198455301537793') return this.sendErrorMessage(interaction, { errorType: ErrorTypes.COMMAND_FAILURE, errorMessage: 'Unauthorized'})
+        else {
+            const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+            const array: unknown[] = [];
+
+            this.client.commands.forEach((command) => {
+                array.push(command.generateSlashCommand());
+            })
+
+            try {
+                this.client.logger.info('Started refreshing application (/) commands');
+
+                await rest.put(
+                    Routes.applicationGuildCommands(this.client.user.id, process.env.DEVELOPER_GUILD),
+                    { body: array },
+                );
+
+                this.client.logger.info('Successfully reloaded application (/) commands.');
+                this.reply(interaction, { content: ':thumbsup: Successfully Reloaded application (/) commands.', ephemeral: true})
+            }
+            catch (error) {
+                this.client.logger.error(error);
+                this.sendErrorMessage(interaction, { errorType: ErrorTypes.EXTERNAL_ERROR, errorMessage: error.stack})
+            }
+        }
+    }
+}
